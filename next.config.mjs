9690f6946273d3b7842e0ad85d41f1fb8e2ade34
vova <your.email@example.com>
2024-10-26 18:32:10 +0300

    try one

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,13 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      path: false,
-      crypto: false,
-      stream: false,
-      zlib: false,
-      http: false,
-      https: false,
-      tls: false,
-      net: false,
-      child_process: false,
-    };
-    return config;
+  images: {
+    unoptimized: true,
   },
-};
 
-export default nextConfig;
+}
+
+export default nextConfig
